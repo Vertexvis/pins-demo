154d15f945ed432421868b2446a0107b594dff0d
Madison Ehlers <madison.ehlers@vertexvis.com>
2024-03-05 13:16:09 -0600

    [PLAT-4007] Updating Demo Stream Key

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -31,7 +31,7 @@ export const Config: Configuration = {
 // Wind Turbine
 export const DefaultCredentials: StreamCredentials = {
   clientId: "08F675C4AACE8C0214362DB5EFD4FACAFA556D463ECA00877CB225157EF58BFA",
-  streamKey: "OHCC5wLnH9SISYJQcaprKZrSbhKU7FSmBAea",
+  streamKey: "WV-ClJ1PSfiT3Zwj5OZTu-L1dJjIPo9aTDg8",
 };
 
 export function head<T>(items?: T | T[]): T | undefined {
